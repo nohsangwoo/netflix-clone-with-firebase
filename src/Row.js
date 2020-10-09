30b1013e5c366a90def55a05535dbebc6c4ec407
nohsangwoo <nsgr12@naver.com>
2020-10-09 16:12:26 +0900

    add trailer popup

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row(props) {
   const { title, fetchUrl, isLargeRow } = props;
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -16,6 +19,35 @@ function Row(props) {
     }
     fetchData();
   }, [fetchUrl]);
+
+  const opts = {
+    heght: "390",
+    width: "100%",
+    playerVars: {
+      // ,2:45:08
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      // movieTrailer 라는 api로 해당 영화의 이름으로 검색된 유튜브 주소를 가져옴
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          // https://www.youtube.com/watch?time_continue=1&v=QWbMckU3AOQ&feature=emb_title 이런식의 주소에서
+          // 필요없는 정보는 다 지우고 파라미터값만 가져옴
+          // v=QWbMckU3AOQ  이런걸 가져옴
+          const AllurlParms = new URL(url).search;
+
+          // 가져온 파라미터에서 v의 파라미터 값을 가져옴 QWbMckU3AOQ
+          const urlParms = new URLSearchParams(AllurlParms);
+          setTrailerUrl(urlParms.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -30,6 +62,7 @@ function Row(props) {
           return (
             <img
               key={movie.id}
+              onClick={() => handleClick(movie)}
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={potserPath}
               alt={movie.name}
@@ -37,6 +70,7 @@ function Row(props) {
           );
         })}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
